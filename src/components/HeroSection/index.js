dd33ea419b764144b8d0e84652c1af7e128cb039
Istiak Ahmed <istiakahmed1999@gmail.com>
2021-05-25 11:18:45 +0100

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -22,7 +22,7 @@ const HeroSection = () => {
   return (
     <HeroContainer id="home">
       <HeroBg>
-        <VideoBg autoPlay loop muted src={Video} type="vide/mp4" />
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
 
       <HeroContent>
